0a5d4823b194115882668e5aed1f119afb8f2795
misael-gdev <mslsanchez@hotmail.com>
2026-01-30 12:56:06 +0100

    Saving contexts in component for reusing

--- Runtime/utility/management/component/capabilities/bases/ComponentBase.cs
+++ Runtime/utility/management/component/capabilities/bases/ComponentBase.cs
@@ -5,6 +5,8 @@ namespace TSLib.Utility.Management.Component.Capabilities
 {
     public abstract class ComponentBase : MonoBehaviour, IComponent
     {
+        protected SceneCtx SceneCtx { get; set; }
+        protected AppCtx AppCtx { get; set; }
         public virtual void Initialize() { }
         public virtual void Bind(SceneCtx sceneCtx, AppCtx appCtx) { }
         public virtual void Configure() { }
